6b856068a34da653f4a3e01d47985076bb28a1bb
kimeunsur <124233661+kimeunsur@users.noreply.github.com>
2025-01-18 05:03:45 +0900

    5;03

---
+++ back-end/npds/src/main/java/com/example/npds/NpdsApplication.java
@@ -0,0 +1,13 @@
+package com.example.npds;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NpdsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NpdsApplication.class, args);
+	}
+
+}
---
+++ back-end/npds/src/test/java/com/example/npds/NpdsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.npds;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NpdsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
