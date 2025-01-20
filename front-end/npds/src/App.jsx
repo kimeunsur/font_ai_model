76a29cd524c65c62492320246418e6ecd864ab02
Leesengmin1003 <cwn36963@kaist.ac.kr>
2025-01-20 23:33:29 +0900

    23:33 add Layout.jsx for permanent nav bar

--- front-end/npds/src/App.jsx
+++ front-end/npds/src/App.jsx
@@ -1,8 +1,8 @@
 import React from "react";
-import { Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { UserProvider } from "./UserContext";
+import Layout from "../src/components/Layout";
 import StartPage from "./components/StartPage";
-import LoginModal from "./components/LoginModal";
 import MainPage from "./components/MainPage";
 import CreateLetter from "./components/CreateLetter";
 import CreateLetter2 from "./components/CreateLetter2";
@@ -14,12 +14,14 @@ const App = () => {
     <UserProvider>
       <Routes>
         <Route path="/" element={<StartPage />} />
-        <Route path="/loginpage" element={<LoginModal />} />
-        <Route path="/main" element={<MainPage />} />
-        <Route path="/create-letter" element={<CreateLetter />} />
-        <Route path="/create-letter-2" element={<CreateLetter2 />} />
-        <Route path="/my-fonts" element={<MyFonts />} />
-        <Route path="/my-letters" element={<MyLetters />} />
+
+        <Route element={<Layout />}>
+          <Route path="/main" element={<MainPage />} />
+          <Route path="/create-letter" element={<CreateLetter />} />
+          <Route path="/create-letter-2" element={<CreateLetter2 />} />
+          <Route path="/my-fonts" element={<MyFonts />} />
+          <Route path="/my-letters" element={<MyLetters />} />
+        </Route>
       </Routes>
     </UserProvider>
   );
