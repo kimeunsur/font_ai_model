a221067c174408e5a70e898182a3e26fc1a5b5ab
Leesengmin1003 <cwn36963@kaist.ac.kr>
2025-01-21 02:47:57 +0900

    02:47 Only my Fonts~

--- front-end/npds/src/components/MyFonts.jsx
+++ front-end/npds/src/components/MyFonts.jsx
@@ -1,10 +1,29 @@
-import React from "react";
-import "../styles/MyFonts.css"; // CSS 파일을 가져옵니다.
+import React, { useState, useEffect } from "react";
+import testFontData from "../data/testFontData";
+import { useUser } from "../UserContext";
+import "../styles/MyFonts.css";
 
 const MyFonts = () => {
+  const [fonts, setFonts] = useState([]);
+  const { user } = useUser();
+
+  // 더미 데이터를 로드
+  useEffect(() => {
+    setFonts(testFontData); // 데이터를 상태에 저장
+  }, []);
+
   return (
     <main className="main">
-      <h1 className="heading">와! 내 폰트 페이지!</h1>
+      <h1 className="heading">나만의 폰뚜~</h1>
+      <div className="subbox">
+        {fonts.map((font) => (
+          <div key={font.id} className="font-row">
+            <p className="font-name">{font.fontName}</p>
+            <p className="font-text">(대충 뭐 해당하는 폰트가 입혀진 텍스트)</p>
+            <p className="font-date">{new Date(font.createdAt).toLocaleDateString()}</p>
+          </div>
+        ))}
+      </div>
     </main>
   );
 };
