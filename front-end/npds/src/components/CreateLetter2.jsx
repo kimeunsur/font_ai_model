984383a26de25cb8b542302569929ae379383806
Leesengmin1003 <cwn36963@kaist.ac.kr>
2025-01-21 16:24:57 +0900

    16:24 add test TextToImage jpg

--- front-end/npds/src/components/CreateLetter2.jsx
+++ front-end/npds/src/components/CreateLetter2.jsx
@@ -2,9 +2,17 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import "../styles/CreateLetter2.css";
 
+// 이미지 경로를 불러옵니다.
+import letterBackground111 from "../data/letterbackground111.jpg";
+import letterBackground222 from "../data/letterbackground222.jpg";
+import letterBackground333 from "../data/letterbackground333.jpg";
+
 const CreateLetter2 = () => {
   const location = useLocation();
   const { gptResponse, textToImageInput } = location.state || {};
+
+  // 이미지 배열
+  const images = [letterBackground111, letterBackground222, letterBackground333];
 
   return (
     <main className="main">
@@ -25,12 +33,17 @@ const CreateLetter2 = () => {
 
         {/* 두 번째 그룹 */}
         <div className="output-group">
-          <label className="output-label">Text-to-Image Input:</label>
-          {[...Array(3)].map((_, index) => (
-            <p key={index} className="output-text">
-              {textToImageInput || "데이터 없음"}
-            </p>
-          ))}
+          <label className="output-label">Text-to-Image Output:</label>
+          <div className="image-container">
+            {images.map((image, index) => (
+              <img
+                key={index}
+                src={image}
+                alt={`Generated Background ${index + 1}`}
+                className="output-image"
+              />
+            ))}
+          </div>
         </div>
 
         {/* 세 번째 그룹 */}
