aa7141d648db08cb00e774563a6ce1825d0a4dfe
Leesengmin1003 <cwn36963@kaist.ac.kr>
2025-01-21 15:57:27 +0900

    15:57 CreateLetter2.jsx modify

--- front-end/npds/src/components/CreateLetter2.jsx
+++ front-end/npds/src/components/CreateLetter2.jsx
@@ -1,37 +1,44 @@
-import React, { useState } from "react";
+import React from "react";
 import { useLocation } from "react-router-dom";
-import "../styles/CreateLetter2.css"; // CSS 파일을 가져옵니다.
+import "../styles/CreateLetter2.css";
 
 const CreateLetter2 = () => {
   const location = useLocation();
   const { gptResponse, textToImageInput } = location.state || {};
 
-  
   return (
     <main className="main">
       <h1 className="heading">대충 생성된 편지</h1>
 
       <div className="output-section">
+        {/* 첫 번째 그룹 */}
         <div className="output-group">
           <label className="output-label">GPT 생성 답변</label>
           {gptResponse && gptResponse.length > 0 ? (
             gptResponse.map((response, index) => (
-              <p key={index} className="output-text">
-                {`${index + 1}. ${response}`}</p>
+              <p key={index} className="output-text">{`${index + 1}. ${response}`}</p>
             ))
-          ): (<p className="output-text">데이터 없음</p>)}
+          ) : (
+            <p className="output-text">데이터 없음</p>
+          )}
         </div>
 
+        {/* 두 번째 그룹 */}
         <div className="output-group">
           <label className="output-label">Text-to-Image Input:</label>
-          <p className="output-text">{textToImageInput || "데이터 없음"}</p>
-          <p className="output-text">{textToImageInput || "데이터 없음"}</p>
-          <p className="output-text">{textToImageInput || "데이터 없음"}</p>
+          {[...Array(3)].map((_, index) => (
+            <p key={index} className="output-text">
+              {textToImageInput || "데이터 없음"}
+            </p>
+          ))}
         </div>
 
+        {/* 세 번째 그룹 */}
         <div className="output-group">
           <label className="output-label">Input Zzamppong:</label>
-          <p className="output-text">{textToImageInput || "데이터 없음"} 이랑 {textToImageInput || "데이터 없음"}</p>
+          <p className="output-text">
+            {textToImageInput || "데이터 없음"} 이랑 {textToImageInput || "데이터 없음"}
+          </p>
         </div>
       </div>
     </main>
--- front-end/npds/src/data/testFontData.js
+++ front-end/npds/src/data/testFontData.js
@@ -1,4 +1,4 @@
-const testFontData = Array.from({ length: 10 }, (_, i) => ({
+const testFontData = Array.from({ length: 3 }, (_, i) => ({
   id: (i + 1).toString(),
   userId: `user${i + 1}`,
   fontName: `FontName_${i + 1}`,
